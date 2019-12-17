b0c9edf2cce353470e5b47b0ce1de75f7fd8b625
nilankamanoj <nilankaeng16@gmail.com>
2019-12-17 14:22:04 +0530

    test 4

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php
-$err_msg = ''; 
-echo "<br>Attempting message download for $file<br>"; 
-$out = fopen("out.zip","wb");
-    if ($out == FALSE){ 
-      print "File not opened<br>"; 
-      exit; 
-    } 
-    $ch = curl_init(); 
+// create a new cURL resource
+$ch = curl_init();
 
-    curl_setopt($ch, CURLOPT_FILE, $out); 
-    curl_setopt($ch, CURLOPT_HEADER, 0); 
-    curl_setopt($ch, CURLOPT_URL,"https://www.seedr.cc/zip/75041751?st=0f4038df4104f48280ffa65d046b4e9e20fd4750cf4885d9dbfdb0dce193dd2e&e=1576614999"); 
+// set URL and other appropriate options
+curl_setopt($ch, CURLOPT_URL, "https://www.seedr.cc/zip/75041751?st=0f4038df4104f48280ffa65d046b4e9e20fd4750cf4885d9dbfdb0dce193dd2e&e=1576614999");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch, CURLOPT_HEADER, 0);
 
-    curl_exec($ch); 
-    echo "<br>Error is : ".curl_error ( $ch); 
+// grab URL and pass it to the browser
+$out = curl_exec($ch);
 
-    curl_close($ch); 
+// close cURL resource, and free up system resources
+curl_close($ch);
+
+
+$fp = fopen('data.zip', 'w');
+fwrite($fp, $out);
+fclose($fp);
+
 ?>
